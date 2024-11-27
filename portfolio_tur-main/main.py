3aaa1d5b9d4ba1c62415ef2e968447c91d1970e4
Yonca <73234933+yoncasen@users.noreply.github.com>
2024-11-27 19:21:56 +0300

    Add files via upload

---
+++ portfolio_tur-main/main.py
@@ -0,0 +1,26 @@
+# Import
+from flask import Flask, render_template,request, redirect
+
+
+
+app = Flask(__name__)
+
+# İçerik sayfasını çalıştırma
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Dinamik beceriler
+@app.route('/', methods=['POST'])
+def process_form():
+    button_python = request.form.get('button_python')
+    button_discord = request.form.get('button_discord')
+    return render_template('index.html', 
+                           button_python=button_python, 
+                           button_discord = button_discord
+                           )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
